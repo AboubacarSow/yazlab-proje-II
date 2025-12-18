1eef5908733b1050e5195096191dc7f990f70b7c
ABOUBACAR SOW <175620651+AboubacarSow@users.noreply.github.com>
2025-12-18 23:04:59 +0300

    Implement Serilog

--- backend/src/sna-bootstrapper-api/Program.cs
+++ backend/src/sna-bootstrapper-api/Program.cs
@@ -1,10 +1,23 @@
 using Carter;
 using Scalar.AspNetCore;
+using Serilog;
 using sna_application.Extensions;
 using sna_bootstrapper_api.Exceptions.Handlers;
 using sna_infrastructure.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
+
+//Serilg Configuration
+builder.Configuration.AddJsonFile("serilog.json");
+builder.Host.UseSerilog((context, configuration) =>
+    {
+        //To configure minimally serilog, we need to provide these two parameters
+        configuration
+        .ReadFrom.Configuration(context.Configuration);
+        //.MinimumLevel.Override("Microsoft", new LoggingLevelSwitch(LogEventLevel.Warning))
+        //.WriteTo.File(path:"Logs/Restaurant-API-.log", rollingInterval: RollingInterval.Day,rollOnFileSizeLimit:true)//default size:1GB
+        //.WriteTo.Console(outputTemplate :"[{Timestamp:dd:HH:mm:ss} {Level:u3}] {Message:lj} |{SourceContext} | {NewLine}{Exception}");
+    });
 
 // Add services to the container.
 builder.Services.AddControllers();
