f042173a4bd13806da1c00a6c1c1bf09040bdfeb
EmreYildan <emreyildan@gmail.com>
2025-12-28 13:32:09 +0300

    AStar ve Dijkstra için Endpointler olulşturuldu ve AlgoService içinde düzenlendi

---
+++ backend/src/sna-application/Analysis/AStar/AStarHandler.cs
@@ -0,0 +1,28 @@
+using System.Diagnostics;
+using sna_domain.Services;
+
+namespace sna_application.Analysis.AStar;
+
+public record AStarResult(Guid Id, IReadOnlyList<NodeDto> Nodes, double ExecutionTime);
+public record AStarCommand(Guid GraphId, int StartNodeId, int TargetNodeId) : IRequest<AStarResult>;
+
+internal class AStarHandler(IGraphRepository graphRepository, ILogger<AStarHandler> logger)
+    : IRequestHandler<AStarCommand, AStarResult>
+{
+    public async Task<AStarResult> Handle(AStarCommand request, CancellationToken cancellationToken)
+    {
+        var graph = await graphRepository.GetGraphByIdAsync(request.GraphId, false)
+            ?? throw new NotFoundException($"Graph with Id {request.GraphId} not found");
+        var startNode = graph.GetNodeFromGraphById(request.StartNodeId)
+            ?? throw new NotFoundException($"Node: {request.StartNodeId}", graph.Title);
+        var targetNode = graph.GetNodeFromGraphById(request.TargetNodeId)
+            ?? throw new NotFoundException($"Node: {request.TargetNodeId}", graph.Title);
+        var timer = new Stopwatch();
+        timer.Start();
+        var path = GraphAlgorithmService.AStar(graph, startNode, targetNode);
+        timer.Stop();
+        logger.LogInformation("AStar Algorithm executed in : {Elapsed} ms", timer.ElapsedMilliseconds);
+        var nodeDtos = path.Adapt<IReadOnlyList<NodeDto>>();
+        return new AStarResult(request.GraphId, nodeDtos, timer.ElapsedMilliseconds);
+    }
+}
---
+++ backend/src/sna-application/Analysis/Dijkstra/DijkstraHandler.cs
@@ -0,0 +1,28 @@
+using System.Diagnostics;
+using sna_domain.Services;
+
+namespace sna_application.Analysis.Dijkstra;
+
+public record DijkstraResult(Guid Id, IReadOnlyList<NodeDto> Nodes, double ExecutionTime);
+public record DijkstraCommand(Guid GraphId, int StartNodeId, int TargetNodeId) : IRequest<DijkstraResult>;
+
+internal class DijkstraHandler(IGraphRepository graphRepository, ILogger<DijkstraHandler> logger)
+    : IRequestHandler<DijkstraCommand, DijkstraResult>
+{
+    public async Task<DijkstraResult> Handle(DijkstraCommand request, CancellationToken cancellationToken)
+    {
+        var graph = await graphRepository.GetGraphByIdAsync(request.GraphId, false)
+            ?? throw new NotFoundException($"Graph with Id {request.GraphId} not found");
+        var startNode = graph.GetNodeFromGraphById(request.StartNodeId)
+            ?? throw new NotFoundException($"Node: {request.StartNodeId}", graph.Title);
+        var targetNode = graph.GetNodeFromGraphById(request.TargetNodeId)
+            ?? throw new NotFoundException($"Node: {request.TargetNodeId}", graph.Title);
+        var timer = new Stopwatch();
+        timer.Start();
+        var path = GraphAlgorithmService.Dijkstra(graph, startNode, targetNode);
+        timer.Stop();
+        logger.LogInformation("Dijkstra Algorithm executed in : {Elapsed} ms", timer.ElapsedMilliseconds);
+        var nodeDtos = path.Adapt<IReadOnlyList<NodeDto>>();
+        return new DijkstraResult(request.GraphId, nodeDtos, timer.ElapsedMilliseconds);
+    }
+}
